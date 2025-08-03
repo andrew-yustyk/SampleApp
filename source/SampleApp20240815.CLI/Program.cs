bd3cbc385e043807edfc297330fa14852b73a5c1
Andrii Yustyk <a.yustyk+publicgit@gmail.com>
2025-08-04 00:27:45 +0300

    Cleanup unnecessary staff

--- source/SampleApp20240815.CLI/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SampleApp20240815.CLI;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-    }
-}
--- tests/SampleApp20240815.Tests.Unit/DummyTests.cs
+++ tests/SampleApp20240815.Tests.Unit/DummyTests.cs
@@ -7,14 +7,6 @@ public class DummyTests
     [Fact]
     public void Should_Assert_True()
     {
-        const bool expectedValue = true;
-        Assert.True(expectedValue);
-    }
-
-    [Fact]
-    public void Should_Assert_False()
-    {
-        const bool unexpectedValue = false;
-        Assert.False(unexpectedValue);
+        Assert.True(true);
     }
 }
